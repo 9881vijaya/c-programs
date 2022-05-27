1b1cd2e20c05978a4c0215084465336e1f4e200b
9881vijaya <106001619+9881vijaya@users.noreply.github.com>
2022-05-27 11:58:27 -0700

    Add files via upload

--- program1.c
+++ program1.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < 5; iCnt++)
+    {
+        printf("Hello\n");     
+    }
+
+    return 0;
+}
+
+
+
+                                                                
+
+
+
+
+
 
